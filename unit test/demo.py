e7be0fde6cc10cee405efe5c4cc73d7a532162b2
Necdet Ozan Karakullukcu <nkarakullukcu@conestogac.on.ca>
2023-02-13 23:47:29 -0500

    Add your message

---
+++ unit test/demo.py
@@ -0,0 +1,17 @@
+import unittest
+
+from Basic_Test import is_prime
+
+# import the is_prime function
+
+class TestPrime(unittest.TestCase):
+    def test_two(self):
+        self.assertTrue(is_prime(2))
+    def test_five(self):
+        self.assertTrue(is_prime(5))
+    def test_nine(self):
+        self.assertFalse(is_prime(9))
+    def test_eleven(self):
+        self.assertTrue(is_prime(11))
+if __name__=='__main__':
+    unittest.main()
