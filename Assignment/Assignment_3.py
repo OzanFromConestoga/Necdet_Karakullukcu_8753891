80801d026c0d5669a4bfdbe46ee42cd79e97b489
Necdet Ozan Karakullukcu <nkarakullukcu@conestogac.on.ca>
2023-03-31 12:13:13 -0400

    Integration folder is added

---
+++ Assignment/Assignment_3.py
@@ -0,0 +1,94 @@
+# Importing required libraries
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+# Setting up the webdriver
+driver = webdriver.Chrome()
+
+# Navigating to the Amazon.ca homepage
+driver.get("https://www.amazon.ca")
+time.sleep(3)
+
+# Searching for the first product
+search_bar = driver.find_element("id","twotabsearchtextbox")
+search_bar.send_keys("film")
+search_bar.send_keys(Keys.RETURN)
+time.sleep(5)
+
+# Verifying that the search results page has loaded
+assert "film" in driver.title
+
+# Selecting the first product from the search results
+product_link = driver.find_element("xpath","/html/body/div[1]/div[2]/div[1]/div[1]/div/span[1]/div[1]/div[1]/div/div/div[1]/div[2]/div[2]/div[1]/div/a[2]/div/div[1]/img")
+product_link.click()
+
+# Waiting for the product details page to load
+time.sleep(7)
+
+
+# Adding the product to the cart
+add_to_cart_button = driver.find_element("id","add-to-cart-button")
+add_to_cart_button.click()
+
+# Waiting for the cart to update
+time.sleep(5)
+
+# click on no thanks button for warranty coverage
+no_thanks_film = driver.find_element("id","attachSiNoCoverage")
+no_thanks_film.click()
+
+# Waiting for the cart to update
+time.sleep(5)
+
+
+
+
+
+# Verifying that the product has been added to the cart
+cart_count = driver.find_element("id","nav-cart-count")
+assert cart_count.text == "1"
+
+# Going back to the search results page
+driver.back()
+time.sleep(4)
+
+# Searching for the second product
+search_bar = driver.find_element("id","twotabsearchtextbox")
+search_bar.send_keys("DualSense")
+search_bar.send_keys(Keys.RETURN)
+time.sleep(5)
+
+# Verifying that the search results page has loaded
+assert "DualSense" in driver.title
+
+# Selecting the second product from the search results
+Dualsense_link = driver.find_element("xpath","/html/body/div[1]/div[2]/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/div/div/div[1]/span/a/div/img")
+Dualsense_link.click()
+
+# Waiting for the product details page to load
+time.sleep(5)
+
+# Adding the product to the cart
+add_to_cart_button = driver.find_element("id","add-to-cart-button")
+add_to_cart_button.click()
+
+# Waiting for the cart to update
+time.sleep(5)
+
+
+# Verifying that both products have been added to the cart
+cart_count = driver.find_element("id","nav-cart-count")
+assert cart_count.text == "2"
+# Opening the cart
+cart_button = driver.find_element("id", "nav-cart")
+cart_button.click()
+
+# Waiting for the cart to load
+time.sleep(5)
+
+# Verifying that the cart page has loaded
+assert "Amazon.ca Shopping Cart" in driver.title
+
+# Closing the webdriver
+driver.close()
--- Integration/lab_5_selenium.py
+++ Integration/lab_5_selenium.py
@@ -13,46 +13,56 @@ time.sleep(3)
 # Finding the search bar and entering text
 # search_bar = driver.find_element_by_id("id","twotabsearchtextbox")
 search_bar = driver.find_element("id","twotabsearchtextbox")
-search_bar.send_keys("shoes")
+search_bar.send_keys("framed wall art")
 
 # Submitting the search query
 search_bar.send_keys(Keys.RETURN)
 
 # Waiting for the search results page to load
-time.sleep(3)
+time.sleep(4)
 
 # Verifying that the search results page has loaded
-assert "shoes" in driver.title
+assert "framed wall art" in driver.title
 
-# Selecting a laptop from the search results
+# Selecting shoes from the search results
 # laptop_link = driver.find_element_by_css_selector("span[data-component-type='s-product-image'] a")
-laptop_link = driver.find_element("xpath","/html/body/div[1]/div[2]/div[1]/div[1]/div/span[1]/div[1]/div[1]/div/div/div[1]/div[2]/div[2]/div[1]/div/a[2]/div/div[1]/img")
+shoes_link = driver.find_element("xpath","/html/body/div[1]/div[2]/div[1]/div[1]/div/span[1]/div[1]/div[1]/div/div/div[1]/div[2]/div[1]/div/div[1]/a/div/img")
 # laptop_link = driver.find_element("By.CSS_SELECTOR","span[data-component-type='s-product-image'] a")
-laptop_link.click()
+shoes_link.click()
 
-#shoes_link = driver.find_element("id","native_size_name_8")
-
-#shoes_link.click()
-
-# Waiting for the laptop details page to load
 time.sleep(5)
 
-# Verifying that the correct laptop details page has loaded
-# assert "laptop" in driver.title
+# Selecting shoes size from the dropdown menu
+quantity = driver.find_element("id","quantity")
 
-# Adding the laptop to the cart
+quantity.click()
+
+# Locate your mouse to click the correct size of shoes
+locate_quantity = driver.find_element("xpath","/html/body/div[2]/div[2]/div[4]/div[4]/div[1]/div[2]/div/div[1]/div/div/div/form/div/div/div/div/div[3]/div/div[7]/div/div/span/div/div/span/select/option[5]")
+
+time.sleep(2)
+
+locate_quantity.click()
+
+# Waiting for the shoes details page to load
+time.sleep(3)
+
+# Verifying that the correct shoes details page has loaded
+# assert "shoes" in driver.title
+
+# Adding shoes to the cart
 add_to_cart_button = driver.find_element("id","add-to-cart-button")
 add_to_cart_button.click()
 
 # Waiting for the cart to update
 time.sleep(5)
 
-# Clicking on no thanks button
-no_thanks_button= driver.find_element("xpath","/html/body/div[5]/div[3]/div[1]/div/div/div[2]/div[2]/div/div/div[3]/div/span[2]/span/input")
-no_thanks_button.click()
-time.sleep(2)
+# Clicking on go to cart button
+#goto_cart= driver.find_element("id","rpn4f7-iqc02o-yu0rej-96teta")
+#goto_cart.click()
+#time.sleep(3)
 
-# Verifying that the laptop has been added to the cart
+# Verifying that shoes have been added to the cart
 cart_count = driver.find_element("id","nav-cart-count")
 assert cart_count.text == "1"
 
